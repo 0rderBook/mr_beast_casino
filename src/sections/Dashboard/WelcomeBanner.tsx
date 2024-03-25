04381ace11ee59c6c43505203478f02dd0eb4945
0rderBook <163455199+0rderBook@users.noreply.github.com>
2024-03-25 18:30:56 +0300

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -3,13 +3,40 @@ import styled from 'styled-components';
 import backgroundImage from './banner1.png'; // Замените 'your_image.jpg' на путь к вашему изображению
 
 const Buttons = styled.div`
-  /* Стили кнопок */
-`;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
 
-const Welcome = styled.div`
-  /* Стили приветственного баннера */
-  background-image: url(${backgroundImage}); /* Установка вашего изображения в качестве фона */
-`;
+  @media (min-width: 800px) {
+    height: 100%;
+  }
+
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    padding-top: 0!important;
+  }
+
+  & > button {
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    padding: 10px;
+    background: #ffffffdf;
+    transition: background .2s ease;
+    &:hover {
+      background: white;
+    }
+    color: black;
+    cursor: pointer;
+  }
+`
+
 
 export function WelcomeBanner() {
   return (
